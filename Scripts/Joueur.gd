4a676f4555befd50f88167eb3937c89a86f0be46
Charles303 <Charlou190@gmail.com>
2020-11-19 22:12:23 -0500

    mouvements joueur et tir

--- Scripts/Joueur.gd
+++ Scripts/Joueur.gd
@@ -1,8 +1,9 @@
 extends KinematicBody2D
 
+export (PackedScene) var balle
 export var vitesse = 100
 var velocite = Vector2()
-
+onready var canon = $canon
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -14,9 +15,20 @@ func get_input():
 	var recule = Input.is_action_pressed("arriere")
 	var gauche = Input.is_action_pressed("gauche")
 	var droite = Input.is_action_pressed("droite")
+	var shoot = Input.is_action_just_pressed("shoot")
 	
 	if avance:
 		velocite = Vector2(vitesse,0).rotated(rotation)
+	if recule:
+		velocite = Vector2(vitesse,0).rotated(rotation - 3)
+	if gauche:
+		velocite = Vector2(vitesse,0).rotated(rotation - 1.5)
+	if droite:
+		velocite = Vector2(vitesse,0).rotated(rotation + 1.5)
+	if shoot:
+		var b = balle.instance()
+		b.creer(canon.global_position,rotation)
+		get_parent().add_child(b)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
@@ -24,6 +36,6 @@ func _physics_process(delta):
 	get_input()
 	var dir = get_global_mouse_position() - global_position
 	if dir.length() > 10:
-		rotation = dir.angle()
+		rotation = dir.angle() #rotation entre -3 et 3
 		velocite = move_and_slide(velocite)
 	var collision = move_and_collide(velocite*delta)
